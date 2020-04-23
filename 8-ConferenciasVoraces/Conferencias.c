7fbbafdf2edc0d30e716ddca7747c3a6e2d7f177
José Antonio <jocoara1213@gmail.com>
2020-04-23 14:40:01 +0200

    Conferencias finalizado

--- 8-ConferenciasVoraces/Conferencias.c
+++ 8-ConferenciasVoraces/Conferencias.c
@@ -1,4 +1,3 @@
-#include "ivector.h"
 #include "imatriz2d.h"
 #include <stdlib.h>
 #include <stdio.h>
@@ -12,19 +11,6 @@ struct Conferencia
     int fin;
 };
 typedef struct Conferencia Conferencia;
-
-/**
-* Método auxiliar para mostrar el vector.
-*/
-void mostrarVector(ivector vector)
-{
-
-    for (int i = 0; i < TAM; i++)
-    {
-        printf(" %d", vector[i]);
-    }
-    printf("\n");
-}
 
 /**
  * Función de comparación para los elementos del vector. Compara el campo comienzo del struct Conferencia.
@@ -84,8 +70,9 @@ int compareToDuracion(const void *_a, const void *_b)
 void HeuristicaComienzo(Conferencia Conferencias[])
 {
     qsort(Conferencias, TAM, sizeof(Conferencia), compareToComienzo);
-    ivector salas = icreavector(TAM);
-    salas[0] = Conferencias[0].fin;
+    imatriz2d salas = icreamatriz2d(2, TAM);
+    salas[0][0] = Conferencias[0].comienzo;
+    salas[1][0] = Conferencias[0].fin;
     printf("\nSe abre la sala %d para la conferencia %d de %d a %d\n", 0, Conferencias[0].id, Conferencias[0].comienzo, Conferencias[0].fin);
     int salasOcupadas = 1;
 
@@ -94,21 +81,28 @@ void HeuristicaComienzo(Conferencia Conferencias[])
         int j;
         for (j = 0; j < salasOcupadas; j++)
         {
-            if (salas[j] <= Conferencias[i].comienzo)
+            if (salas[0][j] >= Conferencias[i].fin)
             {
-                salas[j] = Conferencias[i].fin;
+                salas[0][j] = Conferencias[i].comienzo;
+                printf("Se apunta la conferencia %d de %d a %d a la sala %d\n", Conferencias[i].id, Conferencias[i].comienzo, Conferencias[i].fin, j);
+                j = salasOcupadas;
+            }
+            else if (salas[1][j] <= Conferencias[i].comienzo)
+            {
+                salas[1][j] = Conferencias[i].fin;
                 printf("Se apunta la conferencia %d de %d a %d a la sala %d\n", Conferencias[i].id, Conferencias[i].comienzo, Conferencias[i].fin, j);
                 j = salasOcupadas;
             }
         }
         if (j - 1 != salasOcupadas)
         {
-            salas[salasOcupadas] = Conferencias[i].fin;
+            salas[0][salasOcupadas] = Conferencias[i].comienzo;
+            salas[1][salasOcupadas] = Conferencias[i].fin;
             printf("Se abre la sala %d para la conferencia %d de %d a %d\n", salasOcupadas, Conferencias[i].id, Conferencias[i].comienzo, Conferencias[i].fin);
             salasOcupadas++;
         }
     }
-    ifreevector(&salas);
+    ifreematriz2d(&salas);
 }
 
 /**
@@ -118,8 +112,9 @@ void HeuristicaComienzo(Conferencia Conferencias[])
 void HeuristicaFin(Conferencia Conferencias[])
 {
     qsort(Conferencias, TAM, sizeof(Conferencia), compareToFin);
-    ivector salas = icreavector(TAM);
-    salas[0] = Conferencias[0].fin;
+    imatriz2d salas = icreamatriz2d(2, TAM);
+    salas[0][0] = Conferencias[0].comienzo;
+    salas[1][0] = Conferencias[0].fin;
     printf("\nSe abre la sala %d para la conferencia %d de %d a %d\n", 0, Conferencias[0].id, Conferencias[0].comienzo, Conferencias[0].fin);
     int salasOcupadas = 1;
 
@@ -128,21 +123,28 @@ void HeuristicaFin(Conferencia Conferencias[])
         int j;
         for (j = 0; j < salasOcupadas; j++)
         {
-            if (salas[j] <= Conferencias[i].comienzo)
+            if (salas[0][j] >= Conferencias[i].fin)
             {
-                salas[j] = Conferencias[i].fin;
+                salas[0][j] = Conferencias[i].comienzo;
+                printf("Se apunta la conferencia %d de %d a %d a la sala %d\n", Conferencias[i].id, Conferencias[i].comienzo, Conferencias[i].fin, j);
+                j = salasOcupadas;
+            }
+            else if (salas[1][j] <= Conferencias[i].comienzo)
+            {
+                salas[1][j] = Conferencias[i].fin;
                 printf("Se apunta la conferencia %d de %d a %d a la sala %d\n", Conferencias[i].id, Conferencias[i].comienzo, Conferencias[i].fin, j);
                 j = salasOcupadas;
             }
         }
         if (j - 1 != salasOcupadas)
         {
-            salas[salasOcupadas] = Conferencias[i].fin;
+            salas[0][salasOcupadas] = Conferencias[i].comienzo;
+            salas[1][salasOcupadas] = Conferencias[i].fin;
             printf("Se abre la sala %d para la conferencia %d de %d a %d\n", salasOcupadas, Conferencias[i].id, Conferencias[i].comienzo, Conferencias[i].fin);
             salasOcupadas++;
         }
     }
-    ifreevector(&salas);
+    ifreematriz2d(&salas);
 }
 
 /**
